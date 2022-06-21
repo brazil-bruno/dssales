59f2444583969d6a3cd24777ba5793565d93067f
bruno-brazil <bruno.brazil2022@gmail.com>
2022-06-21 07:24:44 -0300

    Home, Navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import './styles.css';
 
-const  NavBar = () => {
+const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <h2>DS<span style={{color: "#FF8400"}}>SALES</span></h2>
+          <a href="/">
+            <h2>
+              DS<span style={{ color: "#FF8400" }}>SALES</span>
+            </h2>
+          </a>
         </nav>
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import NavBar from "../../components/NavBar";
 
-const  Home = () => {
+const Home = () => {
   return (
-    <div>
-        <h1>Página Home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">Sales</h1>
+          <p className="lead">
+            Analyze your sales performance from different perspectives
+          </p>
+          <hr />
+          <p>
+            This application consists of displaying a dashboard from data
+            provided by a backend built with Spring Boot.
+          </p>
+          <a className="btn btn-primary btn-lg" href="/dashboard">Access the Dashboard</a>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
